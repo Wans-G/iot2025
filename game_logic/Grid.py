e314e63b94ba446cd6b65bdc3b33e25715897e0b
James Whipple <james.whipple16@gmail.com>
2025-04-19 14:57:26 -0500

    Uploaded game logic

---
+++ game_logic/Grid.py
@@ -0,0 +1,130 @@
+from Pieces import Tile
+from Pieces import Road
+from Pieces import Town
+import numpy as np
+import pygame
+import random
+
+grid:list[Tile] = [[None for i in range(7)] for k in range(7)]
+tileSize = 32
+land = [(0,0),(1,0),(0,1),(-1,1),(-1,0),(0,-1),(1,-1),(2,-1),(2,0),(1,1),(0,2),(-1,2),(-2,2),(-2,1),(-2,0),(-1,-1),(0,-2),(1,-2),(2,-2)]
+ocean = [(3,-2),(3,-1),(3,0),(2,1),(1,2),(0,3),(-1,3),(-2,3),(-3,3),(-3,2),(-3,1),(-3,0),(-2,-1),(-1,-2),(0,-3),(1,-3),(2,-3),(3,-3)]
+position = land + ocean
+tokenOrder = [5,2,6,3,8,10,9,12,11,4,8,10,9,4,5,6,3,11]
+
+test_map = [0,0,0,0,1,1,1,1,2,2,2,2,3,3,3,4,4,4,5]
+
+
+class HexGrid():
+    def __init__(self):
+        self.roads:dict[frozenset:Road] = {}
+        self.towns:dict[frozenset:Town] = {}
+
+    def createGrid(self, center:tuple):
+        i = 0
+        t = 0
+        random.shuffle(test_map)
+        tokenOrder.reverse()
+        for p in land:
+            posX, posY = getGridToScreen(p[0], p[1])
+            rec = test_map[i]
+            self.setGrid(p, Tile((posX + center[0], posY + center[1]), rec, tokenOrder[t] if rec < 5 else 0, str(i)))
+            if (rec < 5):
+                t += 1
+            i += 1
+        for p in ocean:
+            posX, posY = getGridToScreen(p[0], p[1])
+            self.setGrid(p, Tile((posX + center[0], posY + center[1]), 6, 0, str(i)))
+            i += 1
+
+    def addRoad(self, tile1:int, tile2:int, color:int):
+        if (not checkNeighbor(position[tile1], position[tile2])):
+            return
+        pos1 = self.getGrid(position[tile1]).getPosition()
+        pos2 = self.getGrid(position[tile2]).getPosition()
+        if (pos1[1] - pos2[1] == 0):
+            angle = 0
+        else:
+            angle = np.round(np.rad2deg(np.tan((pos1[0] - pos2[0])/(pos1[1] - pos2[1]))) / 30)  * 30 + 90
+        roadPos = ((pos1[0] + pos2[0]) / 2, (pos1[1] + pos2[1]) / 2)
+        self.roads[frozenset({tile1, tile2})] = Road(roadPos, int(angle), color)
+
+    def buildTown(self, tile1:int, tile2:int, tile3:int, color:int) -> bool:
+        if (not self.townOnRoad({tile1,tile2,tile3}, color)):
+            return False
+        return self.addTown(tile1, tile2, tile3, color)
+
+    def addTown(self, tile1:int, tile2:int, tile3:int, color:int) -> bool:
+        if (not checkCorner(position[tile1], position[tile2], position[tile3])):
+            return False
+        if (frozenset({tile1,tile2,tile3}) in self.towns):
+            return False
+        if (len(self.towns) and not self.canPlaceTown([tile1, tile2, tile3])):
+            return False
+        pos1 = self.getGrid(position[tile1]).getPosition()
+        pos2 = self.getGrid(position[tile2]).getPosition()
+        pos3 = self.getGrid(position[tile3]).getPosition()
+        townPos = ((pos1[0] + pos2[0] + pos3[0]) / 3, (pos1[1] + pos2[1] + pos3[1]) / 3)
+        self.towns[frozenset({tile1,tile2,tile3})] = Town(townPos, color)
+        return True
+
+    def townOnRoad(self, pos:list[int], color:int) -> bool:
+        tiles = [frozenset({pos[0], pos[1]}),frozenset({pos[1], pos[2]}),frozenset({pos[0], pos[2]})]
+        return any([r for r in (set(tiles) & set(self.towns)) if r.getColor() == color])
+
+    def canPlaceTown(self, pos:list[int]) -> bool:
+        tiles = [frozenset({pos[0], pos[1]}),frozenset({pos[1], pos[2]}),frozenset({pos[0], pos[2]})]
+        positions:list = [key for key in self.towns.keys() if (key.issuperset(tiles[0]) or key.issuperset(tiles[1]) or key.issuperset(tiles[2])) and key != pos]
+        return len(positions) == 0
+
+    def setGrid(self, pos, tile:Tile):
+        grid[pos[0]+3][pos[1]+3] = tile
+
+    def getGrid(self, pos) -> Tile:
+        return grid[pos[0]+3][pos[1]+3]
+
+    def drawGrid(self, screen:pygame.Surface):
+        for p in position:
+            self.getGrid(p).render(screen)
+
+        for key in self.roads:
+            self.roads[key].render(screen)
+        
+        for key in self.towns:
+            self.towns[key].render(screen)
+
+    def harvestRecources(self, number:int) -> list[list[int]]:
+        if (number == 7 or number < 2 or number > 12):
+            return None
+        tiles = self.getToken(number)
+
+        out = [[0]*5,[0]*5,[0]*5,[0]*5]
+
+        for t in tiles:
+            towns = [p for p in self.towns.keys() if int(t.getNumber()) in p]
+            for p in towns:
+                out[self.towns[p].getColor()][t.getRecource()] += 1
+
+        return out
+
+    def getToken(self, number) -> list[Tile]:
+        return [self.getGrid(p) for p in land if self.getGrid(p).getTokenValue() == number]
+
+def getGridToScreen(q:int, r:int) -> tuple:
+    xPos = int(tileSize * (np.sqrt(3) * float(q)  +  np.sqrt(3)/2.0 * float(r)))
+    yPos = int(tileSize * 3.0/2.0 * float(r))
+    return xPos, yPos
+
+def checkNeighbor(pos1:tuple, pos2:tuple) -> bool:
+    s1 = -(pos1[0] + pos1[1])
+    s2 = -(pos2[0] + pos2[1])
+    return {pos1[0] - pos2[0], pos1[1] - pos2[1], s1 - s2} == {0,1,-1}
+
+def checkCorner(pos1:tuple, pos2:tuple, pos3:tuple) -> bool:
+    s1 = -(pos1[0] + pos1[1])
+    s2 = -(pos2[0] + pos2[1])
+    s3 = -(pos3[0] + pos3[1])
+    n1 = {pos1[0] - pos2[0], pos1[1] - pos2[1], s1 - s2} == {0,1,-1}
+    n2 = {pos2[0] - pos3[0], pos2[1] - pos3[1], s2 - s3} == {0,1,-1}
+    n3 = {pos1[0] - pos3[0], pos1[1] - pos3[1], s1 - s3} == {0,1,-1}
+    return n1 and n2 and n3
---
+++ game_logic/Main.py
@@ -0,0 +1,184 @@
+import pygame
+from Pieces import Tile
+import numpy as np
+from Grid import HexGrid
+import random
+
+screenDimention = (1280, 720)
+center = (screenDimention[0] / 2, screenDimention[1] / 2)
+
+recource = {"Wood": 0, "Sheep": 1, "Wheat": 2, "Brick": 3, "Ore": 4, "Desert": 5}
+color = {"Red": 0, "Orange": 1, "White": 2, "Blue": 3}
+playerColor = ["red", "orange", "white", "blue"]
+
+def main():
+    pygame.init()
+    pygame.font.init()
+
+    screen = pygame.display.set_mode(screenDimention, pygame.RESIZABLE)
+    gameBoard = pygame.Surface((400,400))
+    player0 = pygame.Surface((320,280))
+    player1 = pygame.Surface((320,280))
+    player2 = pygame.Surface((320,280))
+    player3 = pygame.Surface((320,280))
+    clock = pygame.time.Clock()
+    running = True
+
+    game = Game()
+
+    game.placeTown(0, 1, 2, color["Red"])
+    game.placeRoad(2, 1, color["Red"])
+    game.placeRoad(2, 9, color["Red"])
+    game.placeTown(5, 6, 17, color["Red"])
+    game.placeRoad(17, 6, color["Red"])
+    game.placeRoad(17, 18, color["Red"])
+
+    game.placeTown(15, 16, 5, color["Blue"])
+    game.placeRoad(4, 5, color["Blue"])
+    game.placeRoad(15, 5, color["Blue"])
+    game.placeTown(3, 4, 13, color["Blue"])
+    game.placeRoad(13, 4, color["Blue"])
+    game.placeRoad(4, 14, color["Blue"])
+
+    game.placeTown(3, 12, 11, color["Orange"])
+    game.placeRoad(12, 11, color["Orange"])
+    game.placeRoad(12, 26, color["Orange"])
+    game.placeTown(1, 7, 8, color["Orange"])
+    game.placeRoad(7, 8, color["Orange"])
+    game.placeRoad(20, 8, color["Orange"])
+
+    game.placeTown(6, 7, 18, color["White"])
+    game.placeRoad(18, 7, color["White"])
+    game.placeRoad(18, 19, color["White"])
+    game.placeTown(2, 11, 10, color["White"])
+    game.placeRoad(11, 10, color["White"])
+    game.placeRoad(25, 10, color["White"])
+    
+    game.startGame()
+    game.nextTurn()
+
+    while (running):
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
+        
+        screenX, screenY = screen.get_size()
+        center = (screenX / 2, screenY / 2)
+        scaleX, scaleY = (screenX / screenDimention[0] * 2, screenY / screenDimention[1] * 2)
+        
+        gameBoard.fill("blue")
+        player0.fill("grey")
+        player1.fill("grey")
+        player2.fill("grey")
+        player3.fill("grey")
+
+        game.display(gameBoard, [player0,player1,player2,player3])
+
+        displayBoard = pygame.transform.scale_by(gameBoard, min(scaleY, scaleX))
+        displayP3 = pygame.transform.scale_by(player3, min(scaleY, scaleX)/2)
+        displayP2 = pygame.transform.scale_by(player2, min(scaleY, scaleX)/2)
+        displayP1 = pygame.transform.scale_by(player1, min(scaleY, scaleX)/2)
+        displayP0 = pygame.transform.scale_by(player0, min(scaleY, scaleX)/2)
+
+        screen.fill("blue")
+        screen.blit(displayBoard, (center[0] - displayBoard.get_width()/2, center[1] - displayBoard.get_height()/2))
+        screen.blit(displayP0, (0, 0))
+        screen.blit(displayP1, (screenX - displayP1.get_width(), 0))
+        screen.blit(displayP2, (0, screenY - displayP2.get_height()))
+        screen.blit(displayP3, (screenX - displayP3.get_width(), screenY - displayP3.get_height()))
+
+        pygame.display.flip()
+
+        clock.tick(30)  # limits FPS to 60
+
+    pygame.quit()
+
+class Game():
+    def __init__(self):
+        self.setup = True
+        self.currentTurn = 0
+        self.gameGrid = HexGrid()
+        self.gameGrid.createGrid((200,200))
+        self.players = [Player(0), Player(1), Player(2), Player(3)]
+        self.font = pygame.font.SysFont("Ariel", 40)
+
+    def startGame(self):
+        self.setup = False
+
+    def placeRoad(self, tile1:int, tile2:int, player:int) -> bool:
+        if (self.setup):
+            self.gameGrid.addRoad(tile1, tile2, player)
+            return True
+        if (player != self.currentTurn):
+            return False
+        if (self.players[self.currentTurn].payCards([1,0,0,1,0])):
+            self.gameGrid.addRoad(tile1, tile2, player)
+            return True
+        return False
+
+    def placeTown(self, tile1:int, tile2:int, tile3:int, player:int) -> bool:
+        if (self.setup):
+            self.gameGrid.addTown(tile1, tile2, tile3, player)
+            return True
+        if (player != self.currentTurn):
+            return False
+        if (self.players[self.currentTurn].payCards([1,1,1,1,0])):
+            self.gameGrid.addTown(tile1, tile2, tile3, player)
+            return True
+        return False
+
+    def display(self, surface:pygame.Surface, playerSurfaces:list):
+        self.gameGrid.drawGrid(surface)
+        for i in range(4):
+            self.players[i].display(playerSurfaces[i], self.font)
+
+    def nextTurn(self):
+        self.currentTurn = (self.currentTurn + 1) % 4
+        number = roll()
+        print(number)
+        self.distribute(number)
+
+    def distribute(self, num: int):
+        cards = self.gameGrid.harvestRecources(num)
+        if (cards == None):
+            return
+        for i in range(4):
+            self.players[i].addCards(cards[i])
+
+def roll() -> int:
+    return random.randint(1,6) + random.randint(1,6)
+
+class Player():
+    def __init__(self, num:int):
+        self.hand:list[int] = [0,0,0,0,0]
+        self.cardCount:int = 0
+        self.playerNumber:int = num
+
+    def addCards(self, cards:list[int]):
+        for i in range(5):
+            self.hand[i] += cards[i]
+            self.cardCount += cards[i]
+
+    def payCards(self, cardCost:list[int]) -> bool:
+        for i in range(5):
+            if (self.hand[i] < cardCost[i]):
+                return False
+        for i in range(5):
+            self.hand[i] -= cardCost[i]
+            self.cardCount -= cardCost[i]
+
+    def getHand(self) -> list[int]:
+        return self.hand
+    
+    def display(self, screen:pygame.Surface, font:pygame.font):
+        playertext = font.render("Player{}".format(self.playerNumber+1), False, playerColor[self.playerNumber])
+        screen.blit(playertext, (5, 5))
+
+        text = "Wood: {0!s}\nSheep: {1}\nWheat: {2}\nBrick: {3}\nOre: {4}".format(*self.hand)
+        lines = text.splitlines()
+        for i in range(len(lines)):
+            recourceText = font.render(lines[i], False, (0, 0, 0))
+            screen.blit(recourceText, (5, 5 + (i + 1)* (recourceText.get_rect().height + 5)))
+    
+if (__name__ == "__main__"):
+    main()
