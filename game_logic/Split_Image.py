7e0e12b43e40dcbd3731e01c72a0e77015cb10c3
James Whipple <james.whipple16@gmail.com>
2025-04-26 12:18:10 -0500

    split image testing

---
+++ game_logic/Split_Image.py
@@ -0,0 +1,58 @@
+import PIL.Image
+import numpy as np
+import os
+
+tileSize = 77 # vertical tile height / 2
+tile_path = "game_logic/split_img/tiles"# output path
+
+land = [(0,0),(1,0),(0,1),(-1,1),(-1,0),(0,-1),(1,-1),(2,-1),(2,0),(1,1),(0,2),(-1,2),(-2,2),(-2,1),(-2,0),(-1,-1),(0,-2),(1,-2),(2,-2)]
+ocean = [(3,-2),(3,-1),(3,0),(2,1),(1,2),(0,3),(-1,3),(-2,3),(-3,3),(-3,2),(-3,1),(-3,0),(-2,-1),(-1,-2),(0,-3),(1,-3),(2,-3),(3,-3)]
+position = land + ocean
+
+tile_pos = [(0,0)] * len(position)
+town_pos:dict[frozenset:tuple] = {}
+road_pos:dict[frozenset:tuple] = {}
+
+def split(input:str = "test_img.jpg"):
+    im = PIL.Image.open(input)
+
+    # Create tile position array
+    for i in range(len(position)):
+        p = position[i]
+        tile_pos[i] = getGridToScreen(p[0], p[1])
+
+    # Create road position array
+
+    # Tiles and Tokens
+    for i in range(len(tile_pos)):
+        box = centered_box(tile_pos[i], im.size, tileSize*2)
+        tile = im.crop(box)
+        out = os.path.join(tile_path, f'{i}_{input}')
+        tile.save(out)
+
+def centered_box(pos:tuple, img_size:tuple, box_size:int) -> tuple:
+    w, h = img_size
+    x1 = max(0, pos[0] + w/2 - box_size/2)
+    x2 = min(w, pos[0] + w/2 + box_size/2)
+    y1 = max(0, pos[1] + h/2 - box_size/2)
+    y2 = min(h, pos[1] + h/2 + box_size/2)
+    return (x1, y1, x2, y2)
+
+def getGridToScreen(q:int, r:int) -> tuple:
+    xPos = int(tileSize * (np.sqrt(3) * float(q)  +  np.sqrt(3)/2.0 * float(r)))
+    yPos = int(tileSize * 3.0/2.0 * float(r))
+    return xPos, yPos
+
+def checkNeighbor(pos1:tuple, pos2:tuple) -> bool:
+    s1 = -(pos1[0] + pos1[1])
+    s2 = -(pos2[0] + pos2[1])
+    return {pos1[0] - pos2[0], pos1[1] - pos2[1], s1 - s2} == {0,1,-1}
+
+def roadPosition(pos1:tuple, pos2:tuple) -> tuple:
+    return ((pos1[0] + pos2[0])//2, (pos1[1] + pos2[1])//2)
+
+def townPosition(pos1:tuple, pos2:tuple, pos3:tuple) -> tuple:
+    return ((pos1[0] + pos2[0] + pos3[0])//3, (pos1[1] + pos2[1] + pos3[1])//3)
+
+if __name__ == "__main__":
+    split()
