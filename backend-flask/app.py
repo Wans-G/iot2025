12a8a0e50da93cb1d77befcb136ec0fae6eca248
Colin Sampey <colinsampey@gmail.com>
2025-04-30 12:51:08 -0500

    Fixed missing imports from game_logic in another folder, and added support for cities

--- backend-flask/app.py
+++ backend-flask/app.py
@@ -1,8 +1,21 @@
 from flask import Flask, jsonify
 import requests
 from flask_cors import CORS
-from Game_Logic import Game
-#from decrypt import Decrypt
+from pathlib import Path
+import importlib.util
+
+#Getting the files from the other library
+current_dir = Path(__file__).resolve().parent
+project_dir = current_dir.parent
+game_logic_folder = project_dir / 'game_logic'
+
+game_logic_mod  = importlib.util.spec_from_file_location("Game_Logic.py", game_logic_folder + "/Game_Logic.py")
+game_logic = importlib.util.module_from_spec(game_logic_mod)
+game_logic_mod.loader.exec_module(game_logic)
+
+decrypt_mod = importlib.util.spec_from_file_location("decrypt.py", project_dir + "/decrypt.py")
+decrypt = importlib.util.module_from_spec(decrypt_mod)
+decrypt_mod.loader.exec_module(decrypt)
 
 player_id = 0
 
@@ -10,7 +23,7 @@ player_id = 0
 app = Flask(__name__)
 CORS(app)
 
-current = Game()
+current = game_logic.Game()
 
 #main
 @app.route('/')
@@ -47,8 +60,7 @@ def city(id):
     global current
     if(current.getTurn() != id):
         return jsonify(action=False)
-    #not seeing cities yet in game logic, will update later
-    attempt=False
+    attempt=current.upgradeCity(id)
     return jsonify(action=attempt)
 
 @app.route('/buy-dev-card/<int:id>')
@@ -83,9 +95,11 @@ def admin():
 
 def camera():
     pass
-    #image_enc = requests.get('http://pi-url-for-photo:3000').content
-    #image = decrypt.decrypt(image_enc)
-    #return image
+'''
+    image_enc = requests.get().content
+    image = decrypt.decrypt(image_enc)
+    return image
+'''
 
 if(__name__ == '__main__'):
     app.run(debug=True)
