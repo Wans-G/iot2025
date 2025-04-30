3e142b8543d71a8f4da657c4927bb4ed6a4aab52
Colin Sampey <colinsampey@gmail.com>
2025-04-30 02:45:02 -0500

    Added functionality for actions, and part of end turn.

--- backend-flask/app.py
+++ backend-flask/app.py
@@ -1,7 +1,8 @@
 from flask import Flask, jsonify
+import requests
 from flask_cors import CORS
 from Game_Logic import Game
-
+#from decrypt import Decrypt
 
 player_id = 0
 
@@ -23,27 +24,52 @@ def joining():
     player_id+=1
     return jsonify(id=current_id)
 
-@app.route('/build-road')
-def road():
-    return ('road')
+@app.route('/build-road/<int:id>')
+def road(id):
+    global current
+    if(current.getTurn() != id):
+        attempt = False
+        return jsonify(action=attempt)
+    attempt = current.buildRoad(id)
+    return jsonify(action=attempt)
 
 
-@app.route('/build-house')
-def house():
-    return ('build-house')
+@app.route('/build-house/<int:id>')
+def house(id):
+    global current
+    if(current.getTurn() != id ):
+        return jsonify(action=False)
+    attempt = current.placeTown(id)
+    return jsonify(action=attempt)
 
-@app.route('/build-city')
-def city():
-    return('build-city')
+@app.route('/build-city/<int:id>')
+def city(id):
+    global current
+    if(current.getTurn() != id):
+        return jsonify(action=False)
+    #not seeing cities yet in game logic, will update later
+    attempt=False
+    return jsonify(action=attempt)
 
-@app.route('/buy-dev-card')
-def dev_card():
-    return("dev-card")
+@app.route('/buy-dev-card/<int:id>')
+def dev_card(id):
+    global current
+    if(current.getTurn() != id ):
+        return jsonify(action=False)
+    attempt = current.buyDevCard(id)
+    return jsonify(action=attempt)
 
-@app.route('/end-turn')
-def end_turn():
-    #insert call to pi
-    pass
+@app.route('/end-turn/<int:id>')
+def end_turn(id):
+    global current
+    if(current.getTurn() != id):
+        return jsonify(action=False)
+    attempt = current.nextTurn()
+    #call to pi here
+    image = camera()
+    #insert call to openai here
+    return jsonify(action=attempt)
+    
 
 @app.route('/game-info')
 def get_game_info():
@@ -54,6 +80,12 @@ def get_game_info():
 def admin():
     return('admin')
 
+
+def camera():
+    pass
+    #image_enc = requests.get('http://pi-url-for-photo:3000').content
+    #image = decrypt.decrypt(image_enc)
+    #return image
 
 if(__name__ == '__main__'):
     app.run(debug=True)
--- frontend/index.js
+++ frontend/index.js
@@ -1,4 +1,4 @@
-server = 'http://127.0.0.1:5000'
+server = 'http://127.0.0.1:5000';
 
 playerId = null;
 
@@ -7,12 +7,18 @@ document.addEventListener('DOMContentLoaded', () => {
     getGameInfo();
 });
 
+document.addEventListener('click', devCard());
+document.addEventListener('click', buildRoad());
+document.addEventListener('click', buildSettlement());
+document.addEventListener('click', buildCity());
+
+
 async function joinGame() {
     try {
         const response = await fetch(`${server}/join-game`);
         const data = await response.json();
         console.log("My player id is: " + data.id);
-        playerId = data.id
+        playerId = data.id;
     } catch (error) {
         console.error("Failed to join game.");
     }
@@ -21,10 +27,76 @@ async function joinGame() {
 async function getGameInfo() {
     try{
         const response = await fetch(`${server}/game-info`);
-        
+
 
     } catch (error){
         console.error("Failed to get game info");
     }
 
 }
+
+async function buildRoad(){
+    try{
+        const response = await fetch(`${server}/build-road/${playerId}`);
+        const data = await response.json();
+        
+        
+        await updateResources();
+
+    
+    }catch(error){
+        console.error("Failed to build road");
+    }
+}
+
+
+async function buildSettlement(){
+    try{
+        const response = await fetch(`${server}/build-house/${playerId}`);
+        const data = await response.json();
+        
+        
+        await updateResources();
+
+    
+    }catch(error){
+        console.error("Failed to build road");
+    }
+}
+
+async function buildCity(){
+    try{
+        const response = await fetch(`${server}/build-city/${playerId}`);
+        const data = await response.json();
+        await updateResources();
+
+    
+    }catch(error){
+        console.error("Failed to build road");
+    }
+}
+
+async function devCard(){
+    try{
+        const response = await fetch(`${server}/buy-dev-card/${playerId}`);
+    }catch(error){
+        console.error("failed to buy dev card");
+    }
+}
+
+async function endTurn(){
+    try{
+        const response = await fetch(`${server}/end-turn/${playerId}`);
+        
+        await updateResources();
+
+
+    }catch(error){
+        console.error("Failed to end turn");
+    }
+}
+
+async function updateResources(){
+    console.log("updated resources");
+}
+
